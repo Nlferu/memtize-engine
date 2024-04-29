33645ecba941ccadf40fab774ddc2a80db889268
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-29 23:29:10 +0200

    initial deploy script added

---
+++ script/DeployDYM.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {DexYourMeme} from "../src/DexYourMeme.sol";
+import {Script, console} from "forge-std/Script.sol";
+
+contract DeployDYM is Script {
+    function run() external {
+        uint256 deployerKey = vm.envUint("AST_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerKey);
+        DexYourMeme dym = new DexYourMeme();
+        console.log("Deployed Dex Youe Meme:", address(dym));
+        vm.stopBroadcast();
+    }
+}
