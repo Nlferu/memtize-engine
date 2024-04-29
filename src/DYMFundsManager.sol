8cc767afc6297ac36d011cacf3c78c036e21213a
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-30 00:35:24 +0200

    initial struct

--- src/DYMFundsManager.sol
+++ src/DYMFundsManager.sol
@@ -5,6 +5,19 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@solmate/src/utils/ReentrancyGuard.sol";
 
 contract DYMFundsManager is Ownable, ReentrancyGuard {
+    /// @dev Variables
+    uint256 private s_totalMemes;
+
+    /// @dev Structs
+    struct Memes {
+        uint256 idToAddressToFunds;
+    }
+
+    /// @dev Mappings
+    mapping(uint256 => Memes) private s_memes;
+
     /// @dev Constructor
     constructor() Ownable(msg.sender) {}
+
+    function createMeme() external {}
 }
