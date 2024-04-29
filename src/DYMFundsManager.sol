c7b864ba9b17e6416ee9796cb1a5386b38d0f520
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-30 00:08:20 +0200

    necessary packages imported

--- src/DYMFundsManager.sol
+++ src/DYMFundsManager.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract DYMFundsManager {}
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@solmate/src/utils/ReentrancyGuard.sol";
+
+contract DYMFundsManager is Ownable, ReentrancyGuard {
+    /// @dev Constructor
+    constructor() Ownable(msg.sender) {}
+}
