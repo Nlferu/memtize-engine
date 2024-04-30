3e4cb4a0d29e2704cf9021968e6357f12093f215
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-30 19:49:28 +0200

    struct update

--- src/MemeCoinMinter.sol
+++ src/MemeCoinMinter.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract MemeCoinMinter {}
+import {CoinTemplate} from "./CoinTemplate.sol";
+
+contract MemeCoinMinter {
+    event TokenCreated(address tokenAddress, string tokenName, string tokenSymbol);
+
+    function mintToken(string memory name, string memory symbol, address[] memory recipients, uint256[] memory amounts) external {
+        CoinTemplate newToken = new CoinTemplate(name, symbol, recipients, amounts);
+
+        emit TokenCreated(address(newToken), name, symbol);
+    }
+}
