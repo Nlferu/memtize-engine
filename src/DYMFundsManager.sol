059f190b280b94eeb6cfe9b2565b4255fdf159b3
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-02 21:57:49 +0200

    uint256 renamed into uint

--- src/DYMFundsManager.sol
+++ src/DYMFundsManager.sol
@@ -16,11 +16,11 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
     error DFM__MinterCallFailed();
 
     /// @dev Constants
-    uint256 private constant SUPPLY = 1000000;
-    uint256 private constant HYPER = 1 ether;
+    uint private constant SUPPLY = 1000000;
+    uint private constant HYPER = 1 ether;
 
     /// @dev Variables
-    uint256 private s_totalMemes;
+    uint private s_totalMemes;
 
     /// @dev Enums
     enum MemeStatus {
@@ -33,22 +33,22 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
         address idToCreator;
         string idToName;
         string idToSymbol;
-        uint256 idToTimeLeft;
-        uint256 idToTotalFunds;
+        uint idToTimeLeft;
+        uint idToTotalFunds;
         address[] idToFunders;
-        mapping(address => uint256) idToFunderToFunds;
+        mapping(address => uint) idToFunderToFunds;
         MemeStatus idToMemeStatus;
     }
 
     /// @dev Mappings
-    mapping(uint256 => Meme) private s_memes;
-    mapping(address => uint256) private s_funderToFunds;
+    mapping(uint => Meme) private s_memes;
+    mapping(address => uint) private s_funderToFunds;
 
     /// @dev Events
     event MemeCreated(address indexed creator, string name, string symbol);
-    event MemeFunded(uint256 id, uint256 value);
-    event RefundPerformed(address funder, uint256 amount);
-    event MemeKilled(uint256 id);
+    event MemeFunded(uint id, uint value);
+    event RefundPerformed(address funder, uint amount);
+    event MemeKilled(uint id);
     event MemeHyped(uint id);
 
     /// @dev Constructor
@@ -72,19 +72,19 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
 
     /** @notice Sends request to MCM for creation of meme */
     /** @param id Meme id that we want to work with */
-    function hypeMeme(uint256 id, address memeCoinMinter) external {
+    function hypeMeme(uint id, address memeCoinMinter) external {
         Meme storage meme = s_memes[id];
         if (meme.idToMemeStatus == MemeStatus.DEAD) revert DFM__MemeDead();
 
         address[] memory recipients = meme.idToFunders;
-        uint256[] memory amounts = new uint256[](recipients.length);
+        uint[] memory amounts = new uint[](recipients.length);
 
         for (uint i = 0; i < recipients.length; i++) {
             amounts[i] = meme.idToFunderToFunds[recipients[i]];
         }
 
         (bool success, ) = memeCoinMinter.call{value: meme.idToTotalFunds}(
-            abi.encodeWithSignature("mintToken(string,string,address[],uint256[])", meme.idToName, meme.idToSymbol, recipients, amounts)
+            abi.encodeWithSignature("mintToken(string,string,address[],uint[])", meme.idToName, meme.idToSymbol, recipients, amounts)
         );
 
         if (!success) revert DFM__MinterCallFailed();
@@ -96,15 +96,15 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
 
     /** @notice If meme fails to achieve fund goal on time this function will assign funds back to funders wallets and change state of meme to dead */
     /** @param id Meme id that we want to work with */
-    function killMeme(uint256 id) external {
+    function killMeme(uint id) external {
         Meme storage meme = s_memes[id];
         if (meme.idToMemeStatus == MemeStatus.DEAD) revert DFM__MemeDead();
 
         address[] memory funders = meme.idToFunders;
 
-        for (uint256 i = 0; i < funders.length; i++) {
+        for (uint i = 0; i < funders.length; i++) {
             address funder = funders[i];
-            uint256 funds = meme.idToFunderToFunds[funder];
+            uint funds = meme.idToFunderToFunds[funder];
 
             s_funderToFunds[funder] += funds;
             meme.idToFunderToFunds[funder] = 0;
@@ -117,7 +117,7 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
 
     /** @notice Allows to send funds for given meme */
     /** @param id Meme id that we want to work with */
-    function fundMeme(uint256 id) external payable {
+    function fundMeme(uint id) external payable {
         if (msg.value <= 0) revert DFM__ZeroAmount();
         if (id >= s_totalMemes) revert DFM__InvalidMeme();
         Meme storage meme = s_memes[id];
@@ -132,7 +132,7 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
 
     /** @notice Allows user to withdraw funds from dead memes if user has any */
     function refund() external nonReentrant {
-        uint256 amount = s_funderToFunds[msg.sender];
+        uint amount = s_funderToFunds[msg.sender];
 
         if (amount > 0) {
             s_funderToFunds[msg.sender] = 0;
@@ -153,19 +153,17 @@ contract DYMFundsManager is Ownable, ReentrancyGuard {
     /** @notice Temporary function for testing purposes -> it should be replaced with GraphQl */
     /** @notice Returns total funds available for refund for given funder */
     /** @param funder wallet address of funder */
-    function getFunderToFunds(address funder) external view returns (uint256) {
+    function getFunderToFunds(address funder) external view returns (uint) {
         return s_funderToFunds[funder];
     }
 
     /** @notice Temporary function for testing purposes -> it should be replaced with GraphQl */
     /** @notice Returns all data associated with given meme */
     /** @param id Meme id that we want to work with */
-    function getMemeData(
-        uint256 id
-    ) external view returns (address, string memory, string memory, uint256, uint256, address[] memory, uint256[] memory funds, MemeStatus) {
+    function getMemeData(uint id) external view returns (address, string memory, string memory, uint, uint, address[] memory, uint[] memory funds, MemeStatus) {
         Meme storage meme = s_memes[id];
 
-        funds = new uint256[](meme.idToFunders.length);
+        funds = new uint[](meme.idToFunders.length);
 
         for (uint i; i < meme.idToFunders.length; i++) {
             funds[i] = meme.idToFunderToFunds[meme.idToFunders[i]];
