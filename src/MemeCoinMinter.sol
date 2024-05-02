cf2489d63e841a5908c93c01593eb34abbc5577e
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-02 18:56:06 +0200

    reader added to minter + mint made payable

--- src/MemeCoinMinter.sol
+++ src/MemeCoinMinter.sol
@@ -3,12 +3,27 @@ pragma solidity ^0.8.24;
 
 import {CoinTemplate} from "./CoinTemplate.sol";
 
+interface IERC20 {
+    /** @notice Allows to transfer tokens from any address to any recipient */
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+
+    /** @notice Allows to transfer tokens from this address to recipient */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /** @notice Allows to check token balance for certain address */
+    function balanceOf(address account) external view returns (uint256);
+}
+
 contract MemeCoinMinter {
     event TokenCreated(address tokenAddress, string tokenName, string tokenSymbol);
 
-    function mintToken(string memory name, string memory symbol, address[] memory recipients, uint256[] memory amounts) external {
+    function mintToken(string memory name, string memory symbol, address[] memory recipients, uint256[] memory amounts) external payable {
         CoinTemplate newToken = new CoinTemplate(name, symbol, recipients, amounts);
 
         emit TokenCreated(address(newToken), name, symbol);
+    }
+
+    function getUserTokenBalance(address user, address token) external view returns (uint256) {
+        return IERC20(token).balanceOf(user);
     }
 }
