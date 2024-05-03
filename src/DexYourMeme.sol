491316cbba8f3b12ee5f7c9aa99d7434a0984214
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-03 17:24:03 +0200

    fallback to receive eth implemented

--- src/DexYourMeme.sol
+++ src/DexYourMeme.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract DexYourMeme {}
+contract DexYourMeme {
+    event FundsReceived(uint indexed amount);
+
+    fallback() external payable {
+        emit FundsReceived(msg.value);
+    }
+}
