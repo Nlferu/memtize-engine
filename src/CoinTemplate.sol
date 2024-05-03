74b59ea6bc6c7e47ef162b9a5f67166035922c34
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-03 14:20:44 +0200

    coin update

--- src/CoinTemplate.sol
+++ src/CoinTemplate.sol
@@ -4,9 +4,50 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CoinTemplate is ERC20 {
-    constructor(string memory name, string memory symbol, address[] memory recipients, uint256[] memory amounts) ERC20(name, symbol) {
-        for (uint256 i = 0; i < recipients.length; i++) {
-            _mint(recipients[i], amounts[i]);
+    error CT__ArraysNotParrarell();
+
+    uint256 private constant TOTAL_SUPPLY = 1_000_000 * (10 ** 18);
+    uint256 private constant CREATOR_PERCENT = 15;
+    uint256 private constant TEAM_PERCENT = 5;
+    uint256 private constant FUNDERS_PERCENT = 35;
+    uint256 private constant LIQUIDITY_POOL_PERCENT = 45;
+
+    address private creatorAddress;
+    address private teamAddress;
+    address private liquidityPoolAddress;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address _creatorAddress,
+        address _teamAddress,
+        address _liquidityPoolAddress,
+        address[] memory funders,
+        uint256[] memory amounts
+    ) ERC20(name, symbol) {
+        if (funders.length != amounts.length) revert CT__ArraysNotParrarell();
+
+        creatorAddress = _creatorAddress;
+        teamAddress = _teamAddress;
+        liquidityPoolAddress = _liquidityPoolAddress;
+
+        uint256 totalContributions = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
+            totalContributions += amounts[i];
+        }
+
+        /// @dev Minting tokens for the creator, team, and liquidity pool
+        _mint(creatorAddress, (TOTAL_SUPPLY * CREATOR_PERCENT) / 100);
+        _mint(teamAddress, (TOTAL_SUPPLY * TEAM_PERCENT) / 100);
+        _mint(liquidityPoolAddress, (TOTAL_SUPPLY * LIQUIDITY_POOL_PERCENT) / 100);
+
+        /// @dev Minting tokens for funders proportionally to their contributions
+        if (totalContributions > 0) {
+            uint256 fundersTokens = (TOTAL_SUPPLY * FUNDERS_PERCENT) / 100;
+            for (uint256 i = 0; i < funders.length; i++) {
+                uint256 funderTokens = (fundersTokens * amounts[i]) / totalContributions;
+                _mint(funders[i], funderTokens);
+            }
         }
     }
 }
