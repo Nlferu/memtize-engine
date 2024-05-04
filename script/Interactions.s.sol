faadae904433ca6ed4d8c202f37276e9932bb99f
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-04 20:07:17 +0200

    refactor needed

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -34,6 +34,10 @@ contract CreatePool is Script {
     address private constant DYM_ADDRESS = 0x5f101cdB70bB7081D8AEa072c4E43c6f046A76fE;
     address private constant MEME_COIN = 0x04d2ead8945cF1186Ec4a35AC9eaFe59B109f17d;
 
+    function run() external {
+        pool();
+    }
+
     function pool() internal {
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
@@ -44,21 +48,17 @@ contract CreatePool is Script {
         console.log("Pool created successfully!");
         vm.stopBroadcast();
     }
-
-    function run() external {
-        pool();
-    }
 }
 
 contract CheckTokenBalance is Script {
-    address private constant WETH_ADDRESS = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
+    address private constant TOKEN_ADDRESS = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
 
     function checkBalance(address token) internal view returns (uint) {
         return IERC20(token).balanceOf(address(this));
     }
 
     function run() external view returns (uint) {
-        uint256 balance = checkBalance(WETH_ADDRESS);
+        uint256 balance = checkBalance(TOKEN_ADDRESS);
 
         console.log("Balance Of WETH: ", balance);
 
