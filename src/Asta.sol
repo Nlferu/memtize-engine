3c950c2be7f3516f52e36476a1310f3dd6656bdd
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-06 18:46:49 +0200

    supply fixed

--- src/Asta.sol
+++ src/Asta.sol
@@ -5,7 +5,8 @@ import {ERC20} from "@solmate/tokens/ERC20.sol";
 
 /// @notice Example ERC20 token for testing
 contract Asta is ERC20 {
-    uint256 private constant supply = 1000000000;
+    /// @dev Minting 1kkk tokens
+    uint256 private constant supply = 1000000000 * 10 ** 18;
 
     constructor() ERC20("Asta", "AST", 18) {
         _mint(msg.sender, supply);
