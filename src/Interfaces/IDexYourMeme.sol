0a8c5d70325b2da854c4c225b408e90b87d315f5
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-08 19:53:45 +0200

    emit renamed

--- src/Interfaces/IDexYourMeme.sol
+++ src/Interfaces/IDexYourMeme.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.24;
 
 interface IDexYourMeme {
-    /// @notice Emitted when liquidity has been provided for token
+    /// @notice Emitted when dexing function executed for token
     /// @param token Meme coin that got dexed
-    event MemeDexedSuccessfully(address indexed token);
+    event MemeDexRequestReceived(address indexed token);
 
     /// @notice Creating, initializing and adding liquidity pool for new meme token
     /// @param memeToken Meme token address to be dexed
