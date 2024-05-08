ea33d5dff6e25157c43761e8494b98c473be1248
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-08 15:05:46 +0200

    lib adjusted to openzeppelin as it is cheaper than solmate

--- src/Asta.sol
+++ src/Asta.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {ERC20} from "@solmate/tokens/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @notice Example ERC20 token for testing
 contract Asta is ERC20 {
     /// @dev Minting 1kkk tokens
-    uint256 private constant supply = 1000000000 * 10 ** 18;
+    uint256 private constant supply = 1_000_000_000 * 10 ** 18;
 
-    constructor() ERC20("Asta", "AST", 18) {
+    constructor() ERC20("Asta", "AST") {
         _mint(msg.sender, supply);
     }
 }
