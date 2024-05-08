50cd1c2469408b0d1305283d6c3eff439c9e3aa8
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-08 15:05:59 +0200

    descriptions and params added

--- src/CoinTemplate.sol
+++ src/CoinTemplate.sol
@@ -10,14 +10,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  */
 
 contract CoinTemplate is ERC20 {
+    /// @dev Errors
     error CT__ArraysNotParrarell();
 
+    /// @dev Constants
     uint256 private constant TOTAL_SUPPLY = 1_000_000 * (10 ** 18);
     uint256 private constant CREATOR_PERCENT = 15;
     uint256 private constant TEAM_PERCENT = 5;
     uint256 private constant FUNDERS_PERCENT = 35;
     uint256 private constant LIQUIDITY_POOL_PERCENT = 45;
 
+    /** @param name Name of new ERC20 Meme Token */
+    /** @param symbol Symbol of new ERC20 Meme Token */
+    /** @param creator Meme creator wallet address */
+    /** @param team Dex Your Meme team wallet address */
+    /** @param recipients Array parallel to 'amounts[]' contains all funders of new ERC20 Meme Token */
+    /** @param amounts Array parallel to 'recipients[]' contains all funds of new ERC20 Meme Token */
+    /** @param totalFunds Sum of ETH gathered for new ERC20 Meme Token */
+    /** @param dym DexYourMeme contract address */
     constructor(
         string memory name,
         string memory symbol,
