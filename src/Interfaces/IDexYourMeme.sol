ee6ec427d278fc7efbec21d3fd0436f510b58c87
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-09 15:14:33 +0200

    documentation updated

--- src/Interfaces/IDexYourMeme.sol
+++ src/Interfaces/IDexYourMeme.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.24;
 
 interface IDexYourMeme {
-    /// @notice Emitted when dexing function executed for token
+    /// @notice Emitted when dexMeme function has been executed for token
     /// @param token Meme coin that got dexed
     event MemeDexRequestReceived(address indexed token);
 
-    /// @notice Creating, initializing and adding liquidity pool for new meme token
+    /// @notice Creates, initializes and adds liquidity pool for new meme token
     /// @param memeToken Meme token address to be dexed
     function dexMeme(address memeToken) external;
 }
