300af5b1dcf807be689ec046aabfd6097cd178b2
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-11 16:12:41 +0200

    deploy script fixed for new contract

--- script/DeployDFM.s.sol
+++ script/DeployDFM.s.sol
@@ -5,11 +5,11 @@ import {DYMFundsManager} from "../src/DYMFundsManager.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract DeployDFM is Script {
-    function run(address team, address mcm, address dym) external {
+    function run(address team, address mcm, uint interval) external {
         uint256 deployerKey = vm.envUint("LOCAL_PRIVATE_KEY");
 
         vm.startBroadcast(deployerKey);
-        DYMFundsManager dfm = new DYMFundsManager(team, mcm, dym);
+        DYMFundsManager dfm = new DYMFundsManager(team, mcm, interval);
         console.log("Deployed Dex Youe Meme:", address(dfm));
         console.log("Owner: ", dfm.owner());
         vm.stopBroadcast();
