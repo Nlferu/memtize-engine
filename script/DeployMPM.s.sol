4d1303d5a777960f1206fd0eafd92e4ad520b49c
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-15 13:19:40 +0200

    script adjusted for tests

--- script/DeployMPM.s.sol
+++ script/DeployMPM.s.sol
@@ -5,13 +5,15 @@ import {MemeProcessManager} from "../src/MemeProcessManager.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract DeployMPM is Script {
-    function run(address team, address mcm, uint interval) external {
-        uint256 deployerKey = vm.envUint("LOCAL_PRIVATE_KEY");
+    function run(address mcm, address mcd, uint interval) external returns (MemeProcessManager) {
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerKey);
-        MemeProcessManager mpm = new MemeProcessManager(team, mcm, interval);
-        console.log("Deployed Meme Process Manager:", address(mpm));
-        console.log("Owner: ", mpm.owner());
+        MemeProcessManager memeProcessManager = new MemeProcessManager(mcm, mcd, interval);
+        console.log("Deployed Meme Process Manager:", address(memeProcessManager));
+        console.log("Owner: ", memeProcessManager.owner());
         vm.stopBroadcast();
+
+        return memeProcessManager;
     }
 }
