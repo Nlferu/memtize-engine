7e3edd30cf95de9d61438470a8ea3a6da9da5019
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-15 13:19:35 +0200

    script adjusted for tests

--- script/DeployMCM.s.sol
+++ script/DeployMCM.s.sol
@@ -5,13 +5,15 @@ import {MemeCoinMinter} from "../src/MemeCoinMinter.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract DeployMCM is Script {
-    function run() external {
-        uint256 deployerKey = vm.envUint("LOCAL_PRIVATE_KEY");
+    function run() external returns (MemeCoinMinter) {
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerKey);
-        MemeCoinMinter mcm = new MemeCoinMinter();
-        console.log("Deployed Meme Coin Minter:", address(mcm));
-        console.log("Owner: ", mcm.owner());
+        MemeCoinMinter memeCoinMinter = new MemeCoinMinter();
+        console.log("Deployed Meme Coin Minter:", address(memeCoinMinter));
+        console.log("Owner: ", memeCoinMinter.owner());
         vm.stopBroadcast();
+
+        return memeCoinMinter;
     }
 }
