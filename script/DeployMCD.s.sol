b71efdd7bbada72a0cc8651799f4c994b76289e1
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-15 13:19:24 +0200

    script adjusted for tests

--- script/DeployMCD.s.sol
+++ script/DeployMCD.s.sol
@@ -5,14 +5,15 @@ import {MemeCoinDexer} from "../src/MemeCoinDexer.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract DeployMCD is Script {
-    function run() external {
+    function run(address memeCoinMinter) external returns (MemeCoinDexer) {
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerKey);
-        address mcm = 0xFb94678b88d20B897ac145C319297E1B30223090; // -> tmp random value
-        MemeCoinDexer mcd = new MemeCoinDexer(mcm);
-        console.log("Deployed Meme Coin Dexer:", address(mcd));
-        console.log("Owner: ", mcd.owner());
+        MemeCoinDexer memeCoinDexer = new MemeCoinDexer(memeCoinMinter);
+        console.log("Deployed Meme Coin Dexer:", address(memeCoinDexer));
+        console.log("Owner: ", memeCoinDexer.owner());
         vm.stopBroadcast();
+
+        return memeCoinDexer;
     }
 }
