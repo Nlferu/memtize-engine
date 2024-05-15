dbf586a89124bf8a76d3d7eb2059c5052c10f662
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-15 18:55:54 +0200

    documentation added

--- src/Interfaces/IMemeCoinMinter.sol
+++ src/Interfaces/IMemeCoinMinter.sol
@@ -8,6 +8,14 @@ interface IMemeCoinMinter {
     /// @param coinSymbol Meme coin symbol
     event MemeCoinMinted(address indexed coinAddress, string coinName, string coinSymbol);
 
+    /// @param name Name of new ERC20 Meme Token
+    /// @param symbol Symbol of new ERC20 Meme Token
+    /// @param creator Meme creator wallet address
+    /// @param team Dex Your Meme team wallet address
+    /// @param recipients Array parallel to 'amounts[]' contains all funders of new ERC20 Meme Token
+    /// @param amounts Array parallel to 'recipients[]' contains all funds of new ERC20 Meme Token
+    /// @param totalFunds Sum of ETH gathered for new ERC20 Meme Token
+    /// @param mcd MemeCoinDexer contract address
     struct MintParams {
         string name;
         string symbol;
@@ -16,7 +24,7 @@ interface IMemeCoinMinter {
         address[] recipients;
         uint256[] amounts;
         uint256 totalFunds;
-        address dym;
+        address mcd;
     }
 
     /// @notice Deploys new ERC20 Meme Token
