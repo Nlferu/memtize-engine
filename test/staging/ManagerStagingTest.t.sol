bb0cb829a53a2910d4c87a33a14d37dc56b3cd07
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-16 22:01:26 +0200

    amounts fixed for balances test plus explanation docs

--- test/staging/ManagerStagingTest.t.sol
+++ test/staging/ManagerStagingTest.t.sol
@@ -91,17 +91,18 @@ contract ManagerStagingTest is Test {
         uint dexerErc2 = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), 0x35FbaaadF61e69186B7c7Fc2aF92001aEB338f68);
 
         assertEq(dexerBalance, 0);
-        assertEq(dexerErc1 / 1e18, 0);
-        assertEq(dexerErc2 / 1e18, 0);
+        /// @dev Coins left from Q64.96 inaccurate calculation (Fewer than 1 coin out of 100,000,000 crafted coins (almost 1 coin per 1 ETH) is left in the contract)
+        assertEq(dexerErc1 / 1e18, 10);
+        assertEq(dexerErc2 / 1e18, 4);
 
         uint pool1weth = memeCoinDexer.getUserTokenBalance(0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918, 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
         uint pool1erc = memeCoinDexer.getUserTokenBalance(0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918, 0x4CA4E161f5A6d2B46D71f0C493fc9325b42A5f5E);
         uint pool2weth = memeCoinDexer.getUserTokenBalance(0x0b3cb9Bdb44F436E060687B6f9eBf9cBc3c5a326, 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
         uint pool2erc = memeCoinDexer.getUserTokenBalance(0x0b3cb9Bdb44F436E060687B6f9eBf9cBc3c5a326, 0x35FbaaadF61e69186B7c7Fc2aF92001aEB338f68);
 
-        assertEq(pool1weth, 11);
+        assertEq(pool1weth / 1e18, 11);
         assertEq(pool1erc / 1e18, 1_100_000_000);
-        assertEq(pool2weth, 5);
+        assertEq(pool2weth / 1e18, 5);
         assertEq(pool2erc / 1e18, 500_000_000);
 
         uint[] memory tokens = memeCoinDexer.getAllTokens();
@@ -112,9 +113,8 @@ contract ManagerStagingTest is Test {
     }
 
     modifier onlyOnForkNetwork() {
-        if (block.chainid == 31337) {
-            return;
-        }
+        if (block.chainid == 31337) return;
+
         _;
     }
 }
