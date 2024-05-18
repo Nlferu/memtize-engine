8b80a7f15c92239019dedd9204de8a802b56c8a8
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-18 15:37:46 +0200

    addresses moved to const

--- test/staging/ManagerStagingTest.t.sol
+++ test/staging/ManagerStagingTest.t.sol
@@ -11,6 +11,12 @@ import {DeployMCD} from "../../script/DeployMCD.s.sol";
 import {DeployMPM} from "../../script/DeployMPM.s.sol";
 
 contract ManagerStagingTest is Test {
+    address private constant WETH = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
+    address private constant TOKEN_ONE = 0x4CA4E161f5A6d2B46D71f0C493fc9325b42A5f5E;
+    address private constant TOKEN_TWO = 0x35FbaaadF61e69186B7c7Fc2aF92001aEB338f68;
+    address private constant POOL_ONE = 0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918;
+    address private constant POOL_TWO = 0x0b3cb9Bdb44F436E060687B6f9eBf9cBc3c5a326;
+
     event MemeCreated(uint indexed id, address indexed creator, string name, string symbol);
     event MemeFunded(uint indexed id, uint indexed value);
     event RefundPerformed(address indexed funder, uint indexed amount);
@@ -86,19 +92,19 @@ contract ManagerStagingTest is Test {
 
         memeProcessManager.performUpkeep("");
 
-        uint dexerBalance = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
-        uint dexerErc1 = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), 0x4CA4E161f5A6d2B46D71f0C493fc9325b42A5f5E);
-        uint dexerErc2 = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), 0x35FbaaadF61e69186B7c7Fc2aF92001aEB338f68);
+        uint dexerBalance = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), WETH);
+        uint dexerErc1 = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), TOKEN_ONE);
+        uint dexerErc2 = memeCoinDexer.getUserTokenBalance(address(memeCoinDexer), TOKEN_TWO);
 
         assertEq(dexerBalance, 0);
         /// @dev Coins left from Q64.96 inaccurate calculation (Fewer than 1 coin out of 100,000,000 crafted coins (almost 1 coin per 1 ETH) is left in the contract)
         assertEq(dexerErc1 / 1e18, 10);
         assertEq(dexerErc2 / 1e18, 4);
 
-        uint pool1weth = memeCoinDexer.getUserTokenBalance(0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918, 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
-        uint pool1erc = memeCoinDexer.getUserTokenBalance(0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918, 0x4CA4E161f5A6d2B46D71f0C493fc9325b42A5f5E);
-        uint pool2weth = memeCoinDexer.getUserTokenBalance(0x0b3cb9Bdb44F436E060687B6f9eBf9cBc3c5a326, 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
-        uint pool2erc = memeCoinDexer.getUserTokenBalance(0x0b3cb9Bdb44F436E060687B6f9eBf9cBc3c5a326, 0x35FbaaadF61e69186B7c7Fc2aF92001aEB338f68);
+        uint pool1weth = memeCoinDexer.getUserTokenBalance(POOL_ONE, WETH);
+        uint pool1erc = memeCoinDexer.getUserTokenBalance(POOL_ONE, TOKEN_ONE);
+        uint pool2weth = memeCoinDexer.getUserTokenBalance(POOL_TWO, WETH);
+        uint pool2erc = memeCoinDexer.getUserTokenBalance(POOL_TWO, TOKEN_TWO);
 
         assertEq(pool1weth / 1e18, 11);
         assertEq(pool1erc / 1e18, 1_100_000_000);
