f2fbcd5b72d985a379f3b576be31c459f5bfad7e
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-18 16:08:43 +0200

    path fix

--- test/staging/DexerStagingTest.sol
+++ test/staging/DexerStagingTest.sol
@@ -5,7 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {MemeCoinMinter} from "../../src/MemeCoinMinter.sol";
 import {MemeCoinDexer} from "../../src/MemeCoinDexer.sol";
 import {MemeProcessManager} from "../../src/MemeProcessManager.sol";
-import {InvalidRecipient} from "../mocks/InvalidRecipient.sol";
+import {InvalidRecipient} from "../mock/InvalidRecipient.sol";
 import {DeployMCM} from "../../script/DeployMCM.s.sol";
 import {DeployMCD} from "../../script/DeployMCD.s.sol";
 import {DeployMPM} from "../../script/DeployMPM.s.sol";
