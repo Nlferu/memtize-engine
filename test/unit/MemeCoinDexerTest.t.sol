de65e6b22d5768a05d8522cefc53468718816df5
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-18 17:22:54 +0200

    unused imports removed

--- test/unit/MemeCoinDexerTest.t.sol
+++ test/unit/MemeCoinDexerTest.t.sol
@@ -3,11 +3,8 @@ pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 import {MemeCoinDexer} from "../../src/MemeCoinDexer.sol";
-import {MemeProcessManager} from "../../src/MemeProcessManager.sol";
 import {MemeCoinMinter} from "../../src/MemeCoinMinter.sol";
 import {InvalidRecipient} from "../mock/InvalidRecipient.sol";
-import {DeployMCD} from "../../script/DeployMCD.s.sol";
-import {DeployMPM} from "../../script/DeployMPM.s.sol";
 import {DeployMCM} from "../../script/DeployMCM.s.sol";
 
 contract MemeCoinDexerTest is Test {
