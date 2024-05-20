2a6de9b1607d071e57e32492885b28e36527d2b8
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-20 20:57:19 +0200

    refactored with helperConfig and Network mods

--- script/DeployMCD.s.sol
+++ script/DeployMCD.s.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {MemeCoinDexer} from "../src/MemeCoinDexer.sol";
 import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {MemeCoinDexer} from "../src/MemeCoinDexer.sol";
 
 contract DeployMCD is Script {
     function run(address memeCoinMinter) external returns (MemeCoinDexer) {
-        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        HelperConfig helperConfig = new HelperConfig();
+        (address nftPositionManager, address wrappedNativeToken, , uint256 deployerKey) = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast(deployerKey);
-        MemeCoinDexer memeCoinDexer = new MemeCoinDexer(memeCoinMinter);
+        MemeCoinDexer memeCoinDexer = new MemeCoinDexer(memeCoinMinter, nftPositionManager, wrappedNativeToken);
         console.log("Deployed Meme Coin Dexer:", address(memeCoinDexer));
         console.log("Owner: ", memeCoinDexer.owner());
         vm.stopBroadcast();
