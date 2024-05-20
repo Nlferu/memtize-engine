5643b8b231c79e46f9edd68676e72cbd023634a3
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-20 20:57:24 +0200

    refactored with helperConfig and Network mods

--- script/DeployMPM.s.sol
+++ script/DeployMPM.s.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {MemeProcessManager} from "../src/MemeProcessManager.sol";
 import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {MemeProcessManager} from "../src/MemeProcessManager.sol";
 
 contract DeployMPM is Script {
-    function run(address mcm, address mcd, uint interval) external returns (MemeProcessManager) {
-        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+    uint private constant INTERVAL = 30;
+
+    function run(address mcm, address mcd) external returns (MemeProcessManager) {
+        HelperConfig helperConfig = new HelperConfig();
+        (, , , uint256 deployerKey) = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast(deployerKey);
-        MemeProcessManager memeProcessManager = new MemeProcessManager(mcm, mcd, interval);
+        MemeProcessManager memeProcessManager = new MemeProcessManager(mcm, mcd, INTERVAL);
         console.log("Deployed Meme Process Manager:", address(memeProcessManager));
         console.log("Owner: ", memeProcessManager.owner());
         vm.stopBroadcast();
