bf708cbabebd980b1fdbf66a27d67e1c3aa23f63
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-20 20:57:45 +0200

    refactored with helperConfig and Network mods

--- test/unit/MemeCoinDexerTest.t.sol
+++ test/unit/MemeCoinDexerTest.t.sol
@@ -2,63 +2,45 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {MemeCoinDexer} from "../../src/MemeCoinDexer.sol";
 import {MemeCoinMinter} from "../../src/MemeCoinMinter.sol";
 import {InvalidRecipient} from "../mock/InvalidRecipient.sol";
+import {SkipNetwork} from "../mods/SkipNetwork.sol";
 import {DeployMCM} from "../../script/DeployMCM.s.sol";
 
-contract MemeCoinDexerTest is Test {
+contract MemeCoinDexerTest is Test, SkipNetwork {
     DeployMCM mcmDeployer;
 
+    HelperConfig helperConfig;
     MemeCoinMinter memeCoinMinter;
+    MemeCoinDexer memeCoinDexer;
+
+    address nftPositionManager;
+    address wrappedNativeToken;
 
     function setUp() public {
+        helperConfig;
         mcmDeployer = new DeployMCM();
-
         memeCoinMinter = mcmDeployer.run();
     }
 
-    function test_InitializesDexerCorrectly() public skipFork {
-        MemeCoinDexer mcd = new MemeCoinDexer(address(memeCoinMinter));
-
-        address mcm = mcd.getConstructorData();
+    function test_InitializesDexerCorrectly() public skipForkNetwork {
+        memeCoinDexer = new MemeCoinDexer(address(memeCoinMinter), nftPositionManager, wrappedNativeToken);
+        (address mcm, address nft, address wnt) = memeCoinDexer.getConstructorData();
 
         assertEq(address(memeCoinMinter), mcm, "MCM address not initialized correctly");
+        assertEq(nftPositionManager, nft, "NFT Manager address not initialized correctly");
+        assertEq(wrappedNativeToken, wnt, "Wrapped Native Token address not initialized correctly");
     }
 
-    function test_DexMemeCallableOnlyByMCM() public skipFork {
-        MemeCoinDexer mcd = new MemeCoinDexer(address(memeCoinMinter));
+    function test_DexMemeCallableOnlyByMCM() public skipForkNetwork {
+        memeCoinDexer = new MemeCoinDexer(address(memeCoinMinter), nftPositionManager, wrappedNativeToken);
         address USER = makeAddr("user");
         deal(USER, 100 ether);
 
         vm.prank(USER);
-        vm.expectRevert(MemeCoinDexer.MCD__NotMemeCoinMinterCaller.selector);
-        mcd.dexMeme(USER, 1000, 1000000);
-    }
-
-    function test_CanThrowErrorWhenSwapFails() public skipFork {
-        MemeCoinDexer mcd = new MemeCoinDexer(address(memeCoinMinter));
-
-        deal(address(mcd), 100 ether);
-
-        InvalidRecipient mockContract;
-
-        // Deploy the MockContract and assign it to a WETH address
-        address mockAddress = address(0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14);
-        vm.etch(mockAddress, type(InvalidRecipient).creationCode);
-
-        // Initialize the mockContract instance with the WETH address
-        mockContract = InvalidRecipient(mockAddress);
-
-        vm.prank(address(memeCoinMinter));
-        vm.expectRevert(MemeCoinDexer.MCD__SwapETHFailed.selector);
-        mcd.dexMeme(address(memeCoinMinter), 1000, 1000000);
-    }
-
-    modifier skipFork() {
-        /// @dev Comment below 'if' statement line to perform full coverage test with command 'make testForkSepoliaCoverage'
-        // if (block.chainid != 31337) return;
-
-        _;
+        vm.expectRevert(MemeCoinDexer.MCD__NotAllowedCaller.selector);
+        memeCoinDexer.dexMeme(USER, 1000, 1000000);
     }
 }
