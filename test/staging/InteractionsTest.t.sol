966f3e9be737f717a1355d1490d37ca98448ccd7
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-20 20:57:40 +0200

    refactored with helperConfig and Network mods

--- test/staging/InteractionsTest.t.sol
+++ test/staging/InteractionsTest.t.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {MemeCoinMinter} from "../../src/MemeCoinMinter.sol";
 import {MemeCoinDexer} from "../../src/MemeCoinDexer.sol";
 import {MemeProcessManager} from "../../src/MemeProcessManager.sol";
 import {InvalidRecipient} from "../mock/InvalidRecipient.sol";
+import {SkipNetwork} from "../mods/SkipNetwork.sol";
 import {Collect, DecreaseLiquidity, Burn, GatherCoins} from "../../script/Interactions.s.sol";
 import {DeployMCM} from "../../script/DeployMCM.s.sol";
 import {DeployMCD} from "../../script/DeployMCD.s.sol";
@@ -14,17 +16,16 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ISwapRouter} from "../../src/Interfaces/ISwapRouter.sol";
 import {IUniswapV3Pool} from "../../src/Interfaces/IUniswapV3Pool.sol";
 
-contract InteractionsTest is Test {
-    address private constant WETH = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
+contract InteractionsTest is Test, SkipNetwork {
     address private constant TOKEN = 0x4CA4E161f5A6d2B46D71f0C493fc9325b42A5f5E;
     address private constant POOL = 0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918;
     uint private constant SLIPPAGE = 1;
-    uint private constant INTERVAL = 30;
 
     DeployMCM mcmDeployer;
     DeployMCD mcdDeployer;
     DeployMPM mpmDeployer;
 
+    HelperConfig helperConfig;
     MemeCoinMinter memeCoinMinter;
     MemeCoinDexer memeCoinDexer;
     MemeProcessManager memeProcessManager;
@@ -33,19 +34,24 @@ contract InteractionsTest is Test {
     Burn burn;
     GatherCoins gatherCoins;
 
+    address wrappedNativeToken;
+    address swapRouter;
+
     address private OWNER;
     address private USER = makeAddr("user");
     address private DEVIL = makeAddr("devil");
     uint256 private constant STARTING_BALANCE = 100 ether;
 
     function setUp() public {
+        helperConfig = new HelperConfig();
         mcmDeployer = new DeployMCM();
         mcdDeployer = new DeployMCD();
         mpmDeployer = new DeployMPM();
 
+        (, wrappedNativeToken, swapRouter, ) = helperConfig.activeNetworkConfig();
         memeCoinMinter = mcmDeployer.run();
         memeCoinDexer = mcdDeployer.run(address(memeCoinMinter));
-        memeProcessManager = mpmDeployer.run(address(memeCoinMinter), address(memeCoinDexer), INTERVAL);
+        memeProcessManager = mpmDeployer.run(address(memeCoinMinter), address(memeCoinDexer));
 
         vm.prank(memeCoinMinter.owner());
         memeCoinMinter.transferOwnership(address(memeProcessManager));
@@ -57,7 +63,7 @@ contract InteractionsTest is Test {
         deal(DEVIL, STARTING_BALANCE);
 
         vm.prank(DEVIL);
-        (bool success, ) = WETH.call{value: 50 ether}(abi.encodeWithSignature("deposit()"));
+        (bool success, ) = wrappedNativeToken.call{value: 50 ether}(abi.encodeWithSignature("deposit()"));
         if (success) console.log("Swap for Devil performed successfully!");
 
         collect = new Collect();
@@ -66,13 +72,13 @@ contract InteractionsTest is Test {
         gatherCoins = new GatherCoins();
     }
 
-    function test_Collect() public memesDexedTimePassed swapPerformedToAccumulateFees onlyOnForkNetwork {
+    function test_Collect() public memesDexed swapsPerformed skipLocalNetwork {
         uint[] memory tokens = memeCoinDexer.getAllTokens();
 
         collect.run(address(memeCoinDexer), tokens[0]);
     }
 
-    function test_DecreaseLiquidity() public memesDexedTimePassed onlyOnForkNetwork {
+    function test_DecreaseLiquidity() public memesDexed skipLocalNetwork {
         vm.warp(block.timestamp + 52 weeks);
         vm.roll(block.number + 1);
 
@@ -81,7 +87,7 @@ contract InteractionsTest is Test {
         decreaseLiquidity.run(address(memeCoinDexer), tokens[0], POOL);
     }
 
-    function test_Burn() public memesDexedTimePassed onlyOnForkNetwork {
+    function test_Burn() public memesDexed skipLocalNetwork {
         vm.warp(block.timestamp + 52 weeks);
         vm.roll(block.number + 1);
 
@@ -90,11 +96,11 @@ contract InteractionsTest is Test {
         burn.run(address(memeCoinDexer), tokens[0], POOL);
     }
 
-    function test_GatherCoins() public memesDexedTimePassed onlyOnForkNetwork {
+    function test_GatherCoins() public memesDexed skipLocalNetwork {
         gatherCoins.run(address(memeCoinDexer), TOKEN);
     }
 
-    modifier memesDexedTimePassed() {
+    modifier memesDexed() {
         memeProcessManager.createMeme("Hexur The Memer", "HEX");
 
         vm.prank(USER);
@@ -108,11 +114,8 @@ contract InteractionsTest is Test {
         _;
     }
 
-    modifier swapPerformedToAccumulateFees() {
-        /// @dev Sepolia
-        address swapRouter02 = 0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E;
-
-        bytes memory path = abi.encodePacked(WETH, uint24(3000), TOKEN);
+    modifier swapsPerformed() {
+        bytes memory path = abi.encodePacked(wrappedNativeToken, uint24(3000), TOKEN);
 
         ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
             path: path,
@@ -121,25 +124,25 @@ contract InteractionsTest is Test {
             amountOutMinimum: 1_000_000
         });
 
-        uint wethBal;
+        uint wntBal;
         uint tokenBal;
 
-        wethBal = memeCoinDexer.getUserTokenBalance(DEVIL, WETH);
+        wntBal = memeCoinDexer.getUserTokenBalance(DEVIL, wrappedNativeToken);
         tokenBal = memeCoinDexer.getUserTokenBalance(DEVIL, TOKEN);
-        console.log("Devil WETH Before: ", wethBal / 1e18);
+        console.log("Devil Wrapped Native Token Before: ", wntBal / 1e18);
         console.log("Devil Token Before: ", tokenBal / 1e18);
 
         vm.startPrank(DEVIL);
-        IERC20(WETH).approve(swapRouter02, 1 ether);
-        ISwapRouter(swapRouter02).exactInput(params);
+        IERC20(wrappedNativeToken).approve(swapRouter, 1 ether);
+        ISwapRouter(swapRouter).exactInput(params);
         vm.stopPrank();
 
-        wethBal = memeCoinDexer.getUserTokenBalance(DEVIL, WETH);
+        wntBal = memeCoinDexer.getUserTokenBalance(DEVIL, wrappedNativeToken);
         tokenBal = memeCoinDexer.getUserTokenBalance(DEVIL, TOKEN);
-        console.log("Devil WETH After: ", wethBal / 1e18);
+        console.log("Devil Wrapped Native Token After: ", wntBal / 1e18);
         console.log("Devil Token After: ", tokenBal / 1e18);
 
-        bytes memory path_two = abi.encodePacked(TOKEN, uint24(3000), WETH);
+        bytes memory path_two = abi.encodePacked(TOKEN, uint24(3000), wrappedNativeToken);
 
         ISwapRouter.ExactInputParams memory params_two = ISwapRouter.ExactInputParams({
             path: path_two,
@@ -149,15 +152,9 @@ contract InteractionsTest is Test {
         });
 
         vm.startPrank(DEVIL);
-        IERC20(TOKEN).approve(swapRouter02, 1_000_000 ether);
-        ISwapRouter(swapRouter02).exactInput(params_two);
+        IERC20(TOKEN).approve(swapRouter, 1_000_000 ether);
+        ISwapRouter(swapRouter).exactInput(params_two);
         vm.stopPrank();
-
-        _;
-    }
-
-    modifier onlyOnForkNetwork() {
-        if (block.chainid == 31337) return;
 
         _;
     }
