c5696e5c4d0ef4e09eedcd4bc22c0e42f0b8434c
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-20 20:57:31 +0200

    refactored with helperConfig and Network mods

--- test/mock/InvalidRecipient.sol
+++ test/mock/InvalidRecipient.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract InvalidRecipient {}
+/// @title Dummy contract that always reverts
+/// @notice Used as a placeholder to ensure reverts on attempted calls
+contract InvalidRecipient {
+
+}
