e381e07ddb852970a9646b7dd4989d1cee3be67d
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-20 20:57:29 +0200

    refactored with helperConfig and Network mods

--- src/MemeCoinDexer.sol
+++ src/MemeCoinDexer.sol
@@ -9,24 +9,24 @@ contract MemeCoinDexer is Ownable {
     /// @dev Errors
     error MCD__SwapETHFailed();
     error MCD__DexMemeFailed();
-    error MCD__NotMemeCoinMinterCaller();
+    error MCD__NotAllowedCaller();
     error MCD__NotEnoughTimePassed();
 
     /// @dev Immutables
     address private immutable i_mcm;
+    address private immutable i_nftPositionManager;
+    address private immutable i_wrappedNativeToken;
 
     /// @dev Arrays
     uint[] private s_received_NFTs;
     address[] private s_memeCoinsDexed;
 
     /// @dev Constants
-    address private constant NFT_POSITION_MANAGER = 0x1238536071E1c677A632429e3655c799b22cDA52;
     /** @dev Calculation Formula: ((sqrtPriceX96**2)/(2**192))*(10**(token0 decimals - token1 decimals))
      * This  gives us the price of token0 in token1, where token0 -> meme token ERC20, token1 -> WETH
      */
     /// @dev InitialPrice expression: 1 WETH for 100 000 000 ERC20 | 79228162514264337593543950 -> 1 WETH for 1 000 000 ERC20
     uint160 private constant INITIAL_PRICE = 7922816251426433759354395;
-    address private constant WETH_ADDRESS = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
     uint24 private constant FEE = 3000;
 
     /// @dev Mappings
@@ -39,8 +39,10 @@ contract MemeCoinDexer is Ownable {
     event MemeDexedSuccessfully(address indexed coin, uint indexed nftId);
 
     /// @dev Constructor
-    constructor(address mcm) Ownable(msg.sender) {
+    constructor(address mcm, address nftPositionManager, address wrappedNativeToken) Ownable(msg.sender) {
         i_mcm = mcm;
+        i_nftPositionManager = nftPositionManager;
+        i_wrappedNativeToken = wrappedNativeToken;
     }
 
     //////////////////////////////////// @notice MCD External Functions ////////////////////////////////////
@@ -53,22 +55,23 @@ contract MemeCoinDexer is Ownable {
     /// @notice Swaps ETH into WETH, creates, initializes and adds liquidity pool for new meme token
     /// @param memeCoinAddress Address of ERC20 meme token minted by MCM contract
     function dexMeme(address memeCoinAddress, uint wethAmount, uint memeCoinAmount) external {
-        if (msg.sender != i_mcm) revert MCD__NotMemeCoinMinterCaller();
+        if (msg.sender != i_mcm) revert MCD__NotAllowedCaller();
+
         emit MemeDexRequestReceived(memeCoinAddress);
 
         swapETH(wethAmount);
 
-        /// @dev Creating And Initializing Pool
-        INonfungiblePositionManager(NFT_POSITION_MANAGER).createAndInitializePoolIfNecessary(memeCoinAddress, WETH_ADDRESS, FEE, INITIAL_PRICE);
+        /// @dev Creating and initializing pool
+        INonfungiblePositionManager(i_nftPositionManager).createAndInitializePoolIfNecessary(memeCoinAddress, i_wrappedNativeToken, FEE, INITIAL_PRICE);
 
-        // Approve tokens for the position manager
-        IERC20(WETH_ADDRESS).approve(NFT_POSITION_MANAGER, wethAmount);
-        IERC20(memeCoinAddress).approve(NFT_POSITION_MANAGER, memeCoinAmount);
+        /// @dev Approve tokens for the position manager
+        IERC20(i_wrappedNativeToken).approve(i_nftPositionManager, wethAmount);
+        IERC20(memeCoinAddress).approve(i_nftPositionManager, memeCoinAmount);
 
-        // Add liquidity to the new pool using mint
+        /// @dev Add liquidity to the new pool using mint
         INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
             token0: memeCoinAddress,
-            token1: WETH_ADDRESS,
+            token1: i_wrappedNativeToken,
             fee: FEE, // Fee tier 0.30%
             tickLower: -887220, // Near 0 price
             tickUpper: 887220, // Extremely high price
@@ -80,8 +83,9 @@ contract MemeCoinDexer is Ownable {
             deadline: block.timestamp + 1200 // 20 minutes deadline
         });
 
-        (uint tokenId, , , ) = INonfungiblePositionManager(NFT_POSITION_MANAGER).mint(params);
+        (uint tokenId, , , ) = INonfungiblePositionManager(i_nftPositionManager).mint(params);
 
+        /// @dev Saving dexed coin, it's time left for liquidity pool burn and NFT tokenId
         s_nftToTimeLeft[tokenId] = (block.timestamp + 52 weeks);
         s_memeCoinsDexed.push(memeCoinAddress);
         /// @dev NonfungiblePositionManager is minting NFT directly to this contract, so we do not need 'IERC721Receiver' with 'onERC721Received()'
@@ -94,11 +98,11 @@ contract MemeCoinDexer is Ownable {
 
     /// @notice Swaps ETH for WETH to be able to proceed with 'dexMeme()' function
     function swapETH(uint wethAmount) internal {
-        (bool success, ) = WETH_ADDRESS.call{value: wethAmount}(abi.encodeWithSignature("deposit()"));
+        (bool success, ) = i_wrappedNativeToken.call{value: wethAmount}(abi.encodeWithSignature("deposit()"));
 
         if (!success) revert MCD__SwapETHFailed();
 
-        emit Swapped_ETH_For_WETH(IERC20(WETH_ADDRESS).balanceOf(address(this)));
+        emit Swapped_ETH_For_WETH(IERC20(i_wrappedNativeToken).balanceOf(address(this)));
     }
 
     //////////////////////////////////// @notice DYM Team Functions ////////////////////////////////////
@@ -113,7 +117,7 @@ contract MemeCoinDexer is Ownable {
             amount1Max: type(uint128).max // WETH (type(uint128).max - Gathering all accumulated fees)
         });
 
-        INonfungiblePositionManager(NFT_POSITION_MANAGER).collect(params);
+        INonfungiblePositionManager(i_nftPositionManager).collect(params);
     }
 
     ///************************************************************************************************//
@@ -135,7 +139,7 @@ contract MemeCoinDexer is Ownable {
             deadline: block.timestamp + 1200 // 20 minutes deadline
         });
 
-        INonfungiblePositionManager(NFT_POSITION_MANAGER).decreaseLiquidity(params);
+        INonfungiblePositionManager(i_nftPositionManager).decreaseLiquidity(params);
     }
 
     ///************************************************************************************************//
@@ -146,13 +150,14 @@ contract MemeCoinDexer is Ownable {
     function burn(uint tokenId) external payable onlyOwner {
         if (s_nftToTimeLeft[tokenId] > block.timestamp) revert MCD__NotEnoughTimePassed();
 
-        INonfungiblePositionManager(NFT_POSITION_MANAGER).burn(tokenId);
+        INonfungiblePositionManager(i_nftPositionManager).burn(tokenId);
     }
 
     /// @notice Allows to withdraw all coins pending on contract after pool initialization (Q64.96 price format inaccuracy)
     /// @param coin Address of dexed and burned meme coin or weth
     function gatherCoins(address coin) external onlyOwner {
         IERC20(coin).approve(address(this), IERC20(coin).balanceOf(address(this)));
+
         IERC20(coin).transferFrom(address(this), owner(), IERC20(coin).balanceOf(address(this)));
     }
 
@@ -169,11 +174,14 @@ contract MemeCoinDexer is Ownable {
     }
 
     /// @notice Returns given token balance for certain user
+    /// @param user Address, which we want to check
+    /// @param token Address of token we want to check
     function getUserTokenBalance(address user, address token) external view returns (uint) {
         return IERC20(token).balanceOf(user);
     }
 
-    function getConstructorData() external view returns (address) {
-        return i_mcm;
+    /// @notice Returns constructor immutables
+    function getConstructorData() external view returns (address, address, address) {
+        return (i_mcm, i_nftPositionManager, i_wrappedNativeToken);
     }
 }
