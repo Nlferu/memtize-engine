f7353170afc87a250091cbacc8709612345c788f
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-21 14:49:14 +0200

    refactored test with complete deploy script

--- test/staging/ManagerStagingTest.t.sol
+++ test/staging/ManagerStagingTest.t.sol
@@ -2,38 +2,14 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
+import {DeployDYM} from "../../script/DeployDYM.s.sol";
 import {MemeCoinMinter} from "../../src/MemeCoinMinter.sol";
 import {MemeCoinDexer} from "../../src/MemeCoinDexer.sol";
 import {MemeProcessManager} from "../../src/MemeProcessManager.sol";
-import {InvalidRecipient} from "../mock/InvalidRecipient.sol";
 import {SkipNetwork} from "../mods/SkipNetwork.sol";
-import {DeployMCM} from "../../script/DeployMCM.s.sol";
-import {DeployMCD} from "../../script/DeployMCD.s.sol";
-import {DeployMPM} from "../../script/DeployMPM.s.sol";
 
 contract ManagerStagingTest is Test, SkipNetwork {
-    address private constant TOKEN_ONE = 0x4CA4E161f5A6d2B46D71f0C493fc9325b42A5f5E;
-    address private constant TOKEN_TWO = 0x35FbaaadF61e69186B7c7Fc2aF92001aEB338f68;
-    address private constant POOL_ONE = 0x76e693a8B9C8825bE804CA4e0bEdF9e4D5b92918;
-    address private constant POOL_TWO = 0x0b3cb9Bdb44F436E060687B6f9eBf9cBc3c5a326;
-
-    event MemeCreated(uint indexed id, address indexed creator, string name, string symbol);
-    event MemeFunded(uint indexed id, uint indexed value);
-    event RefundPerformed(address indexed funder, uint indexed amount);
-    event MemeKilled(uint indexed id);
-    event MemeHyped(uint indexed id);
-    event TransferSuccessfull(uint indexed amount);
-    event MemesProcessed(bool indexed performed);
-
-    enum MemeStatus {
-        ALIVE,
-        DEAD
-    }
-
-    DeployMCM mcmDeployer;
-    DeployMCD mcdDeployer;
-    DeployMPM mpmDeployer;
-
+    DeployDYM dymDeployer;
     MemeCoinMinter memeCoinMinter;
     MemeCoinDexer memeCoinDexer;
     MemeProcessManager memeProcessManager;
@@ -45,16 +21,9 @@ contract ManagerStagingTest is Test, SkipNetwork {
     uint256 private constant STARTING_BALANCE = 100 ether;
 
     function setUp() public {
-        mcmDeployer = new DeployMCM();
-        mcdDeployer = new DeployMCD();
-        mpmDeployer = new DeployMPM();
+        dymDeployer = new DeployDYM();
 
-        memeCoinMinter = mcmDeployer.run();
-        memeCoinDexer = mcdDeployer.run(address(memeCoinMinter));
-        memeProcessManager = mpmDeployer.run(address(memeCoinMinter), address(memeCoinDexer));
-
-        vm.prank(memeCoinMinter.owner());
-        memeCoinMinter.transferOwnership(address(memeProcessManager));
+        (memeCoinMinter, memeCoinDexer, memeProcessManager) = dymDeployer.run();
 
         OWNER = memeProcessManager.owner();
 
